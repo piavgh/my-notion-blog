a9d223c4f5b8ff1137debe633dd3c7418232e5df
Hoang Trinh <hoang.trinhj@gmail.com>
2022-12-03 18:23:56 +0700

    refactor: update copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,9 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      <div className={styles.copyright}>
+        Copyright 2018 - {new Date().getFullYear()} {config.author}
+      </div>
 
       <div className={styles.settings}>
         {hasMounted && (
